8a345b4303324730bb068ee897540ed28ae2dbdc
flextremedev <p.wegner1992@gmail.com>
2022-03-18 23:37:57 +0100

    Add page titles

--- src/components/pages/Leasing.tsx
+++ src/components/pages/Leasing.tsx
@@ -17,6 +17,7 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import { useTranslations } from 'next-intl';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 
@@ -77,117 +78,125 @@ export const Leasing = ({ onBack, onNext }: LeasingProps) => {
   );
 
   return (
-    <Layout backgroundImage={<RedCarImage />}>
-      <Stepper activeStep={2} />
-      <Heading
-        as="h1"
-        size="2xl"
-        mt={{ base: 0, md: '1em' }}
-        mb="1em"
-        fontWeight="black"
-      >
-        {t('title')}
-      </Heading>
-      <VStack as="form" spacing={8} onSubmit={submit}>
-        <FormControl isInvalid={Boolean(errors.leasCarPrice)}>
-          <FormLabel htmlFor="leasCarPrice" id="leasCarPriceLabel">
-            {t('carPrice')}
-          </FormLabel>
-          <NumberInput id="leasCarPrice">
-            <NumberInputField
-              {...register('leasCarPrice', {
-                required: tC('errors.emptyString'),
-              })}
-              border="1px solid"
-              borderColor="gray.200"
-            />
-          </NumberInput>
-          <FormErrorMessage>
-            {errors.leasCarPrice && errors.leasCarPrice.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="leasRuntime" id="leasRuntimeLabel">
-            {t('runtime')}
-          </FormLabel>
-          <Slider
-            colorScheme="brand"
-            value={Number(leasRuntime)}
-            min={6}
-            max={60}
-            step={6}
-            aria-labelledby="leasRuntimeLabel"
-            onChange={handleLeasRuntime}
-          >
-            <SliderMark value={60} mt={4} ml={-20} fontSize="sm">
-              {t('runtimeMonths', { monthValue: leasRuntime })}
-            </SliderMark>
-            <SliderTrack>
-              <SliderFilledTrack />
-            </SliderTrack>
-            <SliderThumb />
-          </Slider>
-        </FormControl>
-        <FormControl isInvalid={Boolean(errors.leasMonthlyRate)}>
-          <FormLabel htmlFor="leasMonthlyRate" id="leasMonthlyRateLabel">
-            {t('monthlyRate')}
-          </FormLabel>
-          <NumberInput id="leasMonthlyRate">
-            <NumberInputField
-              {...register('leasMonthlyRate', {
-                required: tC('errors.emptyString'),
-              })}
-              border="1px solid"
-              borderColor="gray.200"
-            />
-          </NumberInput>
-          <FormErrorMessage>
-            {errors.leasMonthlyRate && errors.leasMonthlyRate.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={Boolean(errors.leasInitialPayment)}>
-          <FormLabel htmlFor="leasInitialPayment" id="leasInitialPaymentLabel">
-            {t('initialPayment')}
-          </FormLabel>
-          <NumberInput id="leasInitialPayment">
-            <NumberInputField
-              {...register('leasInitialPayment', {
-                required: tC('errors.emptyString'),
-              })}
-              border="1px solid"
-              borderColor="gray.200"
-            />
-          </NumberInput>
-          <FormErrorMessage>
-            {errors.leasInitialPayment && errors.leasInitialPayment.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={Boolean(errors.leasEndingRate)}>
-          <FormLabel htmlFor="leasEndingRate" id="leasEndingRateLabel">
-            {t('endingRate')}
-          </FormLabel>
-          <NumberInput id="leasEndingRate">
-            <NumberInputField
-              {...register('leasEndingRate', {
-                required: tC('errors.emptyString'),
-              })}
-              border="1px solid"
-              borderColor="gray.200"
-            />
-          </NumberInput>
-          <FormErrorMessage>
-            {errors.leasEndingRate && errors.leasEndingRate.message}
-          </FormErrorMessage>
-        </FormControl>
-        <HStack spacing={4} justify="end" alignSelf="stretch">
-          <Button variant="ghost" colorScheme="brand" onClick={onBack}>
-            {tC('back')}
-          </Button>
-          <Button variant="solid" colorScheme="brand" type="submit">
-            {tC('continue')}
-          </Button>
-        </HStack>
-      </VStack>
-    </Layout>
+    <>
+      <Head>
+        <title>{t('pageTitle')}</title>
+      </Head>
+      <Layout backgroundImage={<RedCarImage />}>
+        <Stepper activeStep={2} />
+        <Heading
+          as="h1"
+          size="2xl"
+          mt={{ base: 0, md: '1em' }}
+          mb="1em"
+          fontWeight="black"
+        >
+          {t('title')}
+        </Heading>
+        <VStack as="form" spacing={8} onSubmit={submit}>
+          <FormControl isInvalid={Boolean(errors.leasCarPrice)}>
+            <FormLabel htmlFor="leasCarPrice" id="leasCarPriceLabel">
+              {t('carPrice')}
+            </FormLabel>
+            <NumberInput id="leasCarPrice">
+              <NumberInputField
+                {...register('leasCarPrice', {
+                  required: tC('errors.emptyString'),
+                })}
+                border="1px solid"
+                borderColor="gray.200"
+              />
+            </NumberInput>
+            <FormErrorMessage>
+              {errors.leasCarPrice && errors.leasCarPrice.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="leasRuntime" id="leasRuntimeLabel">
+              {t('runtime')}
+            </FormLabel>
+            <Slider
+              colorScheme="brand"
+              value={Number(leasRuntime)}
+              min={6}
+              max={60}
+              step={6}
+              aria-labelledby="leasRuntimeLabel"
+              onChange={handleLeasRuntime}
+            >
+              <SliderMark value={60} mt={4} ml={-20} fontSize="sm">
+                {t('runtimeMonths', { monthValue: leasRuntime })}
+              </SliderMark>
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <SliderThumb />
+            </Slider>
+          </FormControl>
+          <FormControl isInvalid={Boolean(errors.leasMonthlyRate)}>
+            <FormLabel htmlFor="leasMonthlyRate" id="leasMonthlyRateLabel">
+              {t('monthlyRate')}
+            </FormLabel>
+            <NumberInput id="leasMonthlyRate">
+              <NumberInputField
+                {...register('leasMonthlyRate', {
+                  required: tC('errors.emptyString'),
+                })}
+                border="1px solid"
+                borderColor="gray.200"
+              />
+            </NumberInput>
+            <FormErrorMessage>
+              {errors.leasMonthlyRate && errors.leasMonthlyRate.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={Boolean(errors.leasInitialPayment)}>
+            <FormLabel
+              htmlFor="leasInitialPayment"
+              id="leasInitialPaymentLabel"
+            >
+              {t('initialPayment')}
+            </FormLabel>
+            <NumberInput id="leasInitialPayment">
+              <NumberInputField
+                {...register('leasInitialPayment', {
+                  required: tC('errors.emptyString'),
+                })}
+                border="1px solid"
+                borderColor="gray.200"
+              />
+            </NumberInput>
+            <FormErrorMessage>
+              {errors.leasInitialPayment && errors.leasInitialPayment.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={Boolean(errors.leasEndingRate)}>
+            <FormLabel htmlFor="leasEndingRate" id="leasEndingRateLabel">
+              {t('endingRate')}
+            </FormLabel>
+            <NumberInput id="leasEndingRate">
+              <NumberInputField
+                {...register('leasEndingRate', {
+                  required: tC('errors.emptyString'),
+                })}
+                border="1px solid"
+                borderColor="gray.200"
+              />
+            </NumberInput>
+            <FormErrorMessage>
+              {errors.leasEndingRate && errors.leasEndingRate.message}
+            </FormErrorMessage>
+          </FormControl>
+          <HStack spacing={4} justify="end" alignSelf="stretch">
+            <Button variant="ghost" colorScheme="brand" onClick={onBack}>
+              {tC('back')}
+            </Button>
+            <Button variant="solid" colorScheme="brand" type="submit">
+              {tC('continue')}
+            </Button>
+          </HStack>
+        </VStack>
+      </Layout>
+    </>
   );
 };
--- src/components/pages/Result.tsx
+++ src/components/pages/Result.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { Box, Button, Flex, Heading, HStack } from '@chakra-ui/react';
 import { useTranslations } from 'next-intl';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import { Layout } from '~/components/Layout';
@@ -113,93 +114,98 @@ export const Result = ({ onBack, onRestart }: ResultProps) => {
   const difference = Number(loserCostsEffective) - Number(winnerCostsEffective);
 
   return (
-    <Layout backgroundImage={<ResultImage />}>
-      <Stepper activeStep={3} />
-      <Heading
-        as="h1"
-        size="2xl"
-        mt={{ base: 0, md: '1em' }}
-        mb="1em"
-        fontWeight="black"
-      >
-        {t('title')}
-      </Heading>
-      <Box borderRadius="md" bgColor="brand.500" color="white" p={6} mb={8}>
-        {t.rich('summary', {
-          winnerMonthlyRate,
-          winnerRuntime,
-          winnerLabel,
-          loserCostsEffective,
-          winnerCostsEffective,
-          difference,
-          loserLabel,
-          strong: (children) => <strong>{children}</strong>,
+    <>
+      <Head>
+        <title>{t('pageTitle')}</title>
+      </Head>
+      <Layout backgroundImage={<ResultImage />}>
+        <Stepper activeStep={3} />
+        <Heading
+          as="h1"
+          size="2xl"
+          mt={{ base: 0, md: '1em' }}
+          mb="1em"
+          fontWeight="black"
+        >
+          {t('title')}
+        </Heading>
+        <Box borderRadius="md" bgColor="brand.500" color="white" p={6} mb={8}>
+          {t.rich('summary', {
+            winnerMonthlyRate,
+            winnerRuntime,
+            winnerLabel,
+            loserCostsEffective,
+            winnerCostsEffective,
+            difference,
+            loserLabel,
+            strong: (children) => <strong>{children}</strong>,
+          })}
+        </Box>
+        <Flex justify="space-between" mb={4}>
+          <Heading as="h2" size="md">
+            {tC('financing')}
+          </Heading>
+          <Heading as="h2" size="md">
+            {tC('leasing')}
+          </Heading>
+        </Flex>
+        {results.map(({ fin, label, leas, isComparable }) => {
+          return (
+            <Box
+              key={label}
+              borderBottom="1px solid"
+              borderBottomColor="gray.200"
+            >
+              <Flex h="16" alignItems="center">
+                <Flex
+                  flex={1}
+                  color={
+                    isComparable === false
+                      ? 'black'
+                      : winner === 'fin'
+                      ? 'green.700'
+                      : 'red.500'
+                  }
+                  data-testid={`${label
+                    .replaceAll(' ', '-')
+                    .replaceAll('.', '')}-${tC('financing')}`.toLowerCase()}
+                >
+                  {fin}
+                </Flex>
+                <Flex>
+                  <Heading as="h3" size="xs">
+                    {label}
+                  </Heading>
+                </Flex>
+                <Flex
+                  flex={1}
+                  color={
+                    isComparable === false
+                      ? 'black'
+                      : winner === 'leas'
+                      ? 'green.700'
+                      : 'red.500'
+                  }
+                  data-testid={`${label
+                    .replaceAll(' ', '-')
+                    .replaceAll('.', '')}-${tC('leasing')}`.toLowerCase()}
+                  justify="flex-end"
+                >
+                  {leas}
+                </Flex>
+              </Flex>
+            </Box>
+          );
         })}
-      </Box>
-      <Flex justify="space-between" mb={4}>
-        <Heading as="h2" size="md">
-          {tC('financing')}
-        </Heading>
-        <Heading as="h2" size="md">
-          {tC('leasing')}
-        </Heading>
-      </Flex>
-      {results.map(({ fin, label, leas, isComparable }) => {
-        return (
-          <Box
-            key={label}
-            borderBottom="1px solid"
-            borderBottomColor="gray.200"
-          >
-            <Flex h="16" alignItems="center">
-              <Flex
-                flex={1}
-                color={
-                  isComparable === false
-                    ? 'black'
-                    : winner === 'fin'
-                    ? 'green.700'
-                    : 'red.500'
-                }
-                data-testid={`${label
-                  .replaceAll(' ', '-')
-                  .replaceAll('.', '')}-${tC('financing')}`.toLowerCase()}
-              >
-                {fin}
-              </Flex>
-              <Flex>
-                <Heading as="h3" size="xs">
-                  {label}
-                </Heading>
-              </Flex>
-              <Flex
-                flex={1}
-                color={
-                  isComparable === false
-                    ? 'black'
-                    : winner === 'leas'
-                    ? 'green.700'
-                    : 'red.500'
-                }
-                data-testid={`${label
-                  .replaceAll(' ', '-')
-                  .replaceAll('.', '')}-${tC('leasing')}`.toLowerCase()}
-                justify="flex-end"
-              >
-                {leas}
-              </Flex>
-            </Flex>
-          </Box>
-        );
-      })}
-      <HStack spacing={4} justify="end" alignSelf="stretch" mt={8}>
-        <Button variant="ghost" colorScheme="brand" onClick={onBack}>
-          {tC('back')}
-        </Button>
-        <Button variant="outline" colorScheme="brand" onClick={onRestart}>
-          {t('restart')}
-        </Button>
-      </HStack>
-    </Layout>
+        <HStack spacing={4} justify="end" alignSelf="stretch" mt={8}>
+          <Button variant="ghost" colorScheme="brand" onClick={onBack}>
+            {tC('back')}
+          </Button>
+          <Button variant="outline" colorScheme="brand" onClick={onRestart}>
+            {t('restart')}
+          </Button>
+        </HStack>
+      </Layout>
+    </>
   );
 };
